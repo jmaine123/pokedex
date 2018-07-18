53f9d1255428337178c003e992efadd9f60ec2aa
Jermaine Smikle <jermaine.smikle@aol.com>
2018-07-18 10:47:38 -0400

    display a pokemon pic

--- main.js
+++ main.js
@@ -26,20 +26,29 @@ class Trainer{
   }
 }
 
+function updateScreen(){}
 
 
 
 
-function loadDoc(url) {
+
+function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      var pic = document.getElementsByTagName('img');
+      var header = document.createElement('h1');
+      header.style.color = 'white';
+      var pic = document.createElement('img');
       var screen = document.getElementById('screen');
       var myObj = JSON.parse(this.responseText);
-      pic.src = myObj.sprites["front_shiny"]
+      // pic.src = myObj.sprites["front_shiny"];
+      pic.src = 'https://orig00.deviantart.net/5c45/f/2015/311/a/9/mega_sharpedo2_by_mz15-d9ftc9p.png'
       screen.appendChild(pic);
-      console.log(screen);
+      screen.style.backgroundColor = 'blue';
+      var txt = document.createTextNode(myObj.name);
+      header.appendChild(txt);
+      screen.appendChild(header);
+      console.log(pic);
     }
   };
   xhttp.open("GET", 'https://pokeapi.co/api/v2/pokemon/319/', true);
